433da51c88fc6123daa6e84a6f13551d3327212e
AirAllergy <henry.c.cui@gmail.com>
2020-04-28 17:04:19 +0100

    Remove the Wales data retrieval bypass

--- scripts/daily_update.py
+++ scripts/daily_update.py
@@ -5,8 +5,6 @@ from plot_ldn_latest import plot_ldn_latest
 
 
 for country in ['Eng', 'Wls', 'Sct', 'Nir']:
-    if country == 'Wls':
-        continue  # temporary override due to the suspended source update
     retrieveData(country)
     parseData(country)
 
